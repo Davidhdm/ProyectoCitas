e982e0e6e368b7ccf4dc7851891483218681aaf4
snejana88 <73829049+snejana88@users.noreply.github.com>
2021-05-17 14:00:33 +0200

    prueba input

---
+++ src/Views/CreateMeeting.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <div class="container">
+    
+        <form action="?action=store" method="post">
+            <input type="text" name="coder" required>
+            <input type="text" name="topic" required>
+            <input type="text" name="mytime" required>
+            <input type="submit" value="Crear nueva cita" >
+            <input type="reset" value="Cancelar la cita">
+            
+
+        </form>
+    </div>
+
+    
+</body>
+</html>
